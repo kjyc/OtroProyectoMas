d8e930b015e8a22e821a16c8c6d0eca2f9d99e16
kevin <joelkevin899@gmail.com>
2021-02-04 16:56:38 +0100

    Versión inicial

---
+++ src/MiPrimeraClase.java
@@ -0,0 +1,13 @@
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es un mensaje");
+		otroMetodo();
+	}
+	
+	private static void otroMetodo() {
+		System.out.println("Otro método");
+	}
+
+}
